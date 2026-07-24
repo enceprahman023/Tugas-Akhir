de18588c0cb0f49d07fd7a4cf58b9a70aee83719
Encep <encep@example.com>
2026-07-24 13:22:43 +0700

    hihi

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-         $middleware->validateCsrfTokens(except: [
-            'pelapor/register',
-            'login',
-        ]);
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckRole::class,
         ]);
